f2e3c2d55b438baa005240ff4d21886e0bc0772e
jaimeeeefer <jaimeferni10@gmail.com>
2025-07-16 22:46:30 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,12 +5,6 @@ import { login, cerrarSesion, verificarSesionGuardada, } from './auth.js';
 import { buscarTren, clearResultados, buscarEstacion, cargarMas } from './api.js';
 import { mostrarTrenAnterior, mostrarTrenSiguiente, autocompletarEstaciones } from './ui.js';
 
-
-function getAuthHeader() {
-  const token = sessionStorage.getItem("authToken");
-  return token ? `Bearer ${token}` : "";
-}
-
 // ALMACENAR ELEMENTOS DEL DOM UNA SOLA VEZ
 const DOMElements = {
     loginButton: document.getElementById("loginButton"),
